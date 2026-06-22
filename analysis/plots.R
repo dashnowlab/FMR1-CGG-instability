0d8c16c7b6991ce836c87af9abaf6b6693d92c3e
Akshay Avvaru <avvaruakshay@gmail.com>
2026-06-22 09:00:55 -0600

    Updating plot R script

--- analysis/plots.R
+++ analysis/plots.R
@@ -10,10 +10,11 @@ theme_set(theme_cowplot())
 # Prepare datasets -------------------------------------------------------------
 
 # Understanding methylation values
-#meth_bins = data.frame(bin = 0:255, min_prob = (0:255)/256, max_prob = ((0:255) + 1)/256)
 
-metadata = read.csv('/Users/avvarua/Documents/projects/diaslab/fmr1-instability/sample-metadata.tsv', sep = '\t')
-all.data = read.csv('/Users/avvarua/Documents/projects/diaslab/allSamples-readsdata.txt', sep = '\t')
+OUT_DIR_FIG <- "figures"
+
+metadata = read.csv('../data/sample-metadata.tsv', sep = '\t')
+all.data = read.csv('../data/allSamples-readsdata.txt', sep = '\t')
 all.data = merge(all.data, metadata, 'bam_id')
 
 all.data = subset(all.data, base_qual > 10)  #filter out reads with basequal <10
@@ -59,9 +60,10 @@ ggplot(male.FXS.data, aes(x = allele_length, fill = prop_meth_level)) +
   facet_wrap(Individual~Region, ncol=3, scales = 'free') + 
   scale_fill_manual(values = c('#50b7bc', '#71c4c8', '#94d3d6', '#b4e0e2', '#daeff1', 'white', '#e6aba0', '#e6aba0', '#dd897a', '#d1644e', '#c74024'))+
   labs(x = 'Allele length (motifs)', y = 'Reads', fill = 'Prop. Cs in repeat methylated')  
-ggsave('../plots/Male_FXS_methylation_prop.pdf', width = 14, height = nrows * 3.6)
+ggsave(paste0(OUT_DIR_FIG, '/Male_FXS_methylation_prop.pdf'), width = 14, height = nrows * 3.6)
 
 # Prop methylation histograms --------------------------------------------------
+
 
 # Inidividual prop methylation -------------------------------------------------
 
@@ -78,10 +80,11 @@ for (individual in individuals){
     scale_fill_manual(values = c('#50b7bc', '#71c4c8', '#94d3d6', '#b4e0e2', '#daeff1', 'white', '#e6aba0', '#e6aba0', '#dd897a', '#d1644e', '#c74024'),
                       breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)) +
     labs(x = 'Allele length', y = 'Reads', fill = 'Prop. Cs in repeat methylated', title = paste0("Individual: ", individual, "    Sex: ", sex)) 
-  ggsave(paste0('../plots/', individual,'_readsPropMeth.jpg'), width=7, height = nrows * 2.6)
+  ggsave(paste0(OUT_DIR_FIG, '/', individual,'_readsPropMeth.jpg'), width=7, height = nrows * 2.6)
 }
 
 # Inidividual prop methylation -------------------------------------------------
+
 
 # Inidividual methylation allele bin -------------------------------------------
 
@@ -94,10 +97,11 @@ for (individual in individuals) {
     scale_x_continuous(breaks=seq(0, 1, by=0.1)) +
     scale_fill_manual(values = c("Normal" = "#00c299", "Premutation" = "#faba42", "Pathogenic" = "#fc584c")) +
     labs(x = 'Proportion of Methylated Cs', y = 'Reads', fill = 'Allele length', title = paste0("Individual: ", individual, "    Sex: ", sex)) 
-  ggsave(paste0('../plots/', individual,'_methvsallelebin.jpg'), width=7)
+  ggsave(paste0(OUT_DIR_FIG, '/', individual,'_methvsallelebin.jpg'), width=7)
 }
 
 # Inidividual methylation allele bin -------------------------------------------
+
 
 # Inidividual methylation allele bin scatter -----------------------------------
 
@@ -109,10 +113,12 @@ for (individual in individuals) {
     geom_point(size = 1.5, alpha = 0.5) + 
     scale_color_manual(values = c("Normal" = "#00c299", "Premutation" = "#faba42", "Pathogenic" = "#fc584c")) +
     labs(x = 'Allele length', y = 'Proportion of Methylated Cs', fill = 'Allele length', title = paste0("Individual: ", individual, "    Sex: ", sex)) 
-  ggsave(paste0('../plots/', individual,'_methvsallelescatter.jpg'), width=7)
+  ggsave(paste0(OUT_DIR_FIG, '/', individual,'_methvsallelescatter.jpg'), width=7)
 }
 
 # Inidividual methylation allele bin scatter -----------------------------------
+
+# Inidividual methylation allele bin histogram ---------------------------------
 
 for (individual in individuals) {
   
@@ -143,14 +149,96 @@ for (individual in individuals) {
   
   # Save plot
   ggsave(
-    paste0('../plots/', individual,'_methvsallelebin.jpg'), 
+    paste0(OUT_DIR_FIG, '/', individual,'_methvsallelebin.jpg'), 
     plot = p,
     width = 7
   )
 }
 
+# Inidividual methylation allele bin histogram ---------------------------------
 
-unmeth.data <- all.data[as.numeric(all.data$prop_meth)<0.5,]
+# Premutation reads vs Methylation bin -----------------------------------------
+
+individual <- "FXS5319"
+ind.data <- subset(all.data, Individual==individual)
+summary.data <- ind.data %>%
+  filter(!is.na(meth_bin)) %>%
+  group_by(Individual, Region, bin, meth_bin) %>%
+  summarise(
+    total_reads = n(),
+    .groups = "drop"
+  ) %>%
+  complete(Individual, Region, meth_bin, fill = list(bin = "Pathogenic", total_reads = 0))
+ggplot(summary.data, aes(x = bin, y = total_reads, fill = meth_bin)) +
+  geom_col(position = position_dodge(width = 0.8), width = 0.7) +
+  facet_wrap(~Region, nrow=2) +
+  scale_fill_manual(values = c("Weak" = "blue", 
+                               "Partial" = "lightblue", 
+                               "Full" = "#fc584c")) +
+  labs (x = "Allele bin", y = "Number of reads", fill = "Methylation", title = individual) +
+  theme_minimal()
+ggsave(paste0(OUT_DIR_FIG, '/',  individual,'-reads-methbin.jpg'), width = 7, height = 4)
+
+# Premutation reads vs Methylation bin -----------------------------------------
+
+# Premutation reads vs Methylation bin stack bar all samples -------------------
+
+summary.data <- all.data %>% filter(!is.na(meth_bin)) %>% filter(bin == "Premutation") %>%
+  group_by(Individual, Sex, Region, bin, meth_bin) %>%
+  summarise( total_reads = n(), .groups = "drop" ) %>%
+  group_by(Individual, Sex, Region, bin) %>%
+  mutate(percentage = total_reads / sum(total_reads) * 100) %>%
+  ungroup()
+  # complete(Individual, Region, meth_bin, fill = list(bin = "Pathogenic", total_reads = 0))
+
+summary.data$label <- paste0(summary.data$Individual," (",summary.data$Region,")")
+write.table(summary.data, file = "/Users/avvarua/Documents/projects/diaslab/plots/MethylationBinPercent-Premutation-Sex.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+
+summary.data <- summary.data %>% filter(Region == "BA10" & (Individual == "FXPM1008-20-RF" | Individual == "FXPM1015-09-DK"))
+
+ggplot(summary.data, aes(x = label, y = percentage, fill = meth_bin)) +
+  geom_col() +
+  facet_wrap(~Sex, nrow = 2, scales = "free") +
+  scale_fill_manual(values = c("Weak" = "blue", "Partial" = "lightblue", "Full" = "#fc584c")) +
+  labs (x = "Samples - Individual (Region)", y = "Percent reads (%)", fill = "Methylation", title = "Percentage of Methylated Premutation reads") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(paste0(OUT_DIR_FIG, '/percent-methbin-permutation-subset.jpg'), width = 5, height = 4)
+
+
+summary.data <- all.data %>% filter(!is.na(meth_bin)) %>% filter(bin == "Premutation") %>%
+  group_by(Individual, Sex, Region, bin, meth_bin) %>%
+  summarise( total_reads = n(), .groups = "drop" ) %>%
+  group_by(Individual, Sex, Region, bin) %>%
+  mutate(percentage = total_reads / sum(total_reads) * 100) %>%
+  ungroup()
+# complete(Individual, Region, meth_bin, fill = list(bin = "Pathogenic", total_reads = 0))
+
+summary.data$label <- paste0(summary.data$Individual," (",summary.data$Region,")")
+ggplot(summary.data, aes(x = label, y = percentage, fill = meth_bin)) +
+  geom_col() +
+  facet_wrap(~Sex, nrow = 2, scales = "free") +
+  scale_fill_manual(values = c("Weak" = "blue", "Partial" = "lightblue", "Full" = "#fc584c")) +
+  labs (x = "Samples - Individual (Region)", y = "Percent reads (%)", fill = "Methylation", title = "Percentage of Methylated Premutation reads") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(paste0(OUT_DIR_FIG, '/percent-methbin-permutation.jpg'), width = 6, height = 8)
+
+# Premutation reads vs Methylation bin stack bar all samples -------------------
+
+# Ridge plot of methylation distribution in premutation samples ----------------
+
+sub.data <- all.data %>% filter(all.data$Sex == "female" & all.data$condition != "FXS")
+sub.data <- sub.data %>% mutate(label = paste0(Individual, "(", Region, ")"))
+ggplot(sub.data, aes(x = prop_meth, y = label, color = bin, fill = bin)) +
+  geom_density_ridges(alpha = 0.5, scale = 1.2) +
+  scale_color_manual(values = c("Normal" = "#00c299",  "Premutation" = "#faba42",  "Pathogenic" = "#fc584c")) +
+  scale_fill_manual(values = c("Normal" = "#00c299",  "Premutation" = "#faba42",  "Pathogenic" = "#fc584c")) +
+  theme_ridges() +
+  theme( axis.title.y = element_blank() ) +
+  labs( x = "Methylation", title = "Methylation Distribution - Males")
+
+# Ridge plot of methylation distribution in premutation samples ----------------
+
+# Allele length histogram colored by expansion range ---------------------------
 
 for (individual in individuals){
   # if (individual == "CON5274") {
@@ -161,13 +249,16 @@ for (individual in individuals){
     scale_fill_manual(values = c("0-54" = "seagreen3", "55-200" = "orangered2")) +
     # scale_fill_manual(values = c('blue4', 'blue3', 'blue2', 'blue1', 'royalblue1', 'snow2', 'tomato', 'red1', 'red2', 'red3', 'red4')) +
     labs(x = 'Allele length (motifs)', y = 'Reads', fill = 'Allele length bin') 
-  ggsave(paste0('../plots/', individual,'_test.jpg'), width=7)
+  ggsave(paste0(OUT_DIR_FIG, '/', individual,'_test.jpg'), width=7)
   # }
 }
 
-for (individual in unique(all.data$Individual)){
-  # if (individual == "FXPM1008-20-RF") {
-  #   print(individual)
+# Allele length histogram colored by expansion range ---------------------------
+
+
+# Combined plots for all samples -----------------------------------------------
+
+for (individual in individuals){
   ggplot(subset(all.data, Individual == individual), aes(x = allele_length, y = prop_meth_level, color = prop_meth_level)) + 
     # geom_histogram(breaks=c(0,55,200)) +
     geom_point(size=2, alpha=0.7) +
@@ -175,22 +266,17 @@ for (individual in unique(all.data$Individual)){
     facet_grid(Region~Individual, scales = 'free') +
     scale_color_manual(values = c('#50b7bc', '#71c4c8', '#94d3d6', '#b4e0e2', '#daeff1', 'white', '#e6aba0', '#e6aba0', '#dd897a', '#d1644e', '#c74024'),
                        breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)) +
-    # xlim(0,200) +
-    # scale_fill_manual(values = c('blue4', 'blue3', 'blue2', 'blue1', 'royalblue1', 'snow2', 'tomato', 'red1', 'red2', 'red3', 'red4')) +
     labs(x = 'Allele length (motifs)', y = 'Reads') 
-  ggsave(paste0('FXS-pureTRGT-plots/', individual,'_FMR1_PureTarget_length-methylation.jpg'), width=7)
-  # }
+  ggsave(paste0(OUT_DIR_FIG, '/', individual,'_FMR1_PureTarget_length-methylation.jpg'), width=7)
 }
 
 for (individual in unique(all.data$Individual)){
-  print(individual)
-  print(subset(all.data, Individual == individual))
   ggplot(subset(all.data, Individual == individual), aes(x = allele_length, fill = prop_meth_level)) + 
     geom_histogram(bins=c(0,55,200)) + 
     facet_grid(Region~Individual, scales = 'free') + 
     scale_fill_manual(values = c('blue4', 'blue3', 'blue2', 'blue1', 'royalblue1', 'snow2', 'tomato', 'red1', 'red2', 'red3', 'red4')) +
     labs(x = 'Allele length (motifs)', y = 'Reads', fill = 'Prop. Cs in repeat methylated') 
-  ggsave(paste0(individual,'_FMR1_PureTarget_methylation_prop.jpg'))
+  ggsave(paste0(OUT_DIR_FIG, '/', individual,'_FMR1_PureTarget_methylation_prop.jpg'))
 }
 
 ggplot(subset(all.data, allele_length > 400), aes(x = allele_length, fill = prop_meth_level)) +
@@ -235,103 +321,6 @@ ggplot(summary.data, aes(x = Region, y = percent_above60, fill = bin)) +
   scale_fill_manual(values = c("Normal" = "#00c299",  "Premutation" = "#faba42",  "Pathogenic" = "#fc584c")) +
   labs (x = "Region", y = "Percent reads (%)", fill = "Allele", title = "Percentage of reads with >=60% of reads methylated FXPM5006") +
   theme_minimal()
-ggsave('/Users/avvarua/Documents/projects/diaslab/plots/FXPM5006-alleles-meth60.jpg', width = 7, height = 4)
+ggsave(paste0(OUT_DIR_FIG, '/FXPM5006-alleles-meth60.jpg'), width = 7, height = 4)
 
-# Premutation reads vs Methylation bin -----------------------------------------
-
-individual <- "FXS5319"
-ind.data <- subset(all.data, Individual==individual)
-summary.data <- ind.data %>%
-  filter(!is.na(meth_bin)) %>%
-  group_by(Individual, Region, bin, meth_bin) %>%
-  summarise(
-    total_reads = n(),
-    .groups = "drop"
-  ) %>%
-  complete(Individual, Region, meth_bin, fill = list(bin = "Pathogenic", total_reads = 0))
-ggplot(summary.data, aes(x = bin, y = total_reads, fill = meth_bin)) +
-  geom_col(position = position_dodge(width = 0.8), width = 0.7) +
-  facet_wrap(~Region, nrow=2) +
-  scale_fill_manual(values = c("Weak" = "blue", 
-                               "Partial" = "lightblue", 
-                               "Full" = "#fc584c")) +
-  labs (x = "Allele bin", y = "Number of reads", fill = "Methylation", title = individual) +
-  theme_minimal()
-ggsave(paste0('/Users/avvarua/Documents/projects/diaslab/plots/',individual,'-reads-methbin.jpg'), width = 7, height = 4)
-
-# Premutation reads vs Methylation bin -----------------------------------------
-
-# Premutation reads vs Methylation bin stack bar all samples -------------------
-
-summary.data <- all.data %>% filter(!is.na(meth_bin)) %>% filter(bin == "Premutation") %>%
-  group_by(Individual, Sex, Region, bin, meth_bin) %>%
-  summarise( total_reads = n(), .groups = "drop" ) %>%
-  group_by(Individual, Sex, Region, bin) %>%
-  mutate(percentage = total_reads / sum(total_reads) * 100) %>%
-  ungroup()
-  # complete(Individual, Region, meth_bin, fill = list(bin = "Pathogenic", total_reads = 0))
-
-summary.data$label <- paste0(summary.data$Individual," (",summary.data$Region,")")
-write.table(summary.data, file = "/Users/avvarua/Documents/projects/diaslab/plots/MethylationBinPercent-Premutation-Sex.tsv", sep="\t", quote=FALSE, row.names = FALSE)
-
-summary.data <- summary.data %>% filter(Region == "BA10" & (Individual == "FXPM1008-20-RF" | Individual == "FXPM1015-09-DK"))
-
-ggplot(summary.data, aes(x = label, y = percentage, fill = meth_bin)) +
-  geom_col() +
-  facet_wrap(~Sex, nrow = 2, scales = "free") +
-  scale_fill_manual(values = c("Weak" = "blue", "Partial" = "lightblue", "Full" = "#fc584c")) +
-  labs (x = "Samples - Individual (Region)", y = "Percent reads (%)", fill = "Methylation", title = "Percentage of Methylated Premutation reads") +
-  theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-ggsave(paste0('/Users/avvarua/Documents/projects/diaslab/plots/percent-methbin-permutation-subset.jpg'), width = 5, height = 4)
-
-
-summary.data <- all.data %>% filter(!is.na(meth_bin)) %>% filter(bin == "Premutation") %>%
-  group_by(Individual, Sex, Region, bin, meth_bin) %>%
-  summarise( total_reads = n(), .groups = "drop" ) %>%
-  group_by(Individual, Sex, Region, bin) %>%
-  mutate(percentage = total_reads / sum(total_reads) * 100) %>%
-  ungroup()
-# complete(Individual, Region, meth_bin, fill = list(bin = "Pathogenic", total_reads = 0))
-
-summary.data$label <- paste0(summary.data$Individual," (",summary.data$Region,")")
-ggplot(summary.data, aes(x = label, y = percentage, fill = meth_bin)) +
-  geom_col() +
-  facet_wrap(~Sex, nrow = 2, scales = "free") +
-  scale_fill_manual(values = c("Weak" = "blue", "Partial" = "lightblue", "Full" = "#fc584c")) +
-  labs (x = "Samples - Individual (Region)", y = "Percent reads (%)", fill = "Methylation", title = "Percentage of Methylated Premutation reads") +
-  theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-ggsave(paste0('/Users/avvarua/Documents/projects/diaslab/plots/percent-methbin-permutation.jpg'), width = 6, height = 8)
-
-# Premutation reads vs Methylation bin stack bar all samples -------------------
-
-sub.data <- all.data %>% filter(all.data$Sex == "female" & all.data$condition != "FXS")
-sub.data <- sub.data %>% mutate(label = paste0(Individual, "(", Region, ")"))
-ggplot(sub.data, aes(x = prop_meth, y = label, color = bin, fill = bin)) +
-  geom_density_ridges(alpha = 0.5, scale = 1.2) +
-  scale_color_manual(values = c("Normal" = "#00c299",  "Premutation" = "#faba42",  "Pathogenic" = "#fc584c")) +
-  scale_fill_manual(values = c("Normal" = "#00c299",  "Premutation" = "#faba42",  "Pathogenic" = "#fc584c")) +
-  theme_ridges() +
-  theme( axis.title.y = element_blank() ) +
-  labs( x = "Methylation", title = "Methylation Distribution - Males")
-
-# for (individual in unique(all.data$Individual)){
-#   ggplot(subset(all.data, Individual == individual), aes(x = allele_length, fill = as.factor(medianmethlevel))) + 
-#     geom_histogram() + 
-#     facet_grid(Region~Individual, scales = 'free') + 
-#     scale_fill_manual(values = c('blue4', 'blue3', 'blue2', 'blue1', 'royalblue1', 'snow2', 'tomato', 'red1', 'red2', 'red3', 'red4')) +
-#     labs(x = 'Allele length (motifs)', y = 'Reads', fill = 'Median methylation') 
-#   ggsave(paste0(individual,'_FMR1_PureTarget_methylation.pdf'))
-# }
-# ggplot(all.data, aes(x = allele_length, fill = as.factor(medianmethlevel))) + 
-#   geom_histogram() + 
-#   facet_wrap(Individual~Region, scales = 'free') + 
-#   scale_fill_manual(values = c('blue4', 'blue3', 'blue2', 'blue1', 'royalblue1', 'snow2', 'tomato', 'red1', 'red2', 'red3', 'red4')) +
-#   labs(x = 'Allele length (motifs)', y = 'Reads', fill = 'Median methylation') 
-# ggsave('FMR1_PureTarget_methylation.pdf', width = 20, height = 20)
-# 
-# ggplot(all.data, aes(x = allele_length, fill = as.factor(medianmethlevel))) + 
-#   geom_histogram(aes(y = after_stat(density))) + 
-#   facet_grid(Region~Individual, scales = 'free') + 
-#   scale_fill_manual(values = c('blue4', 'blue3', 'blue2', 'blue1', 'royalblue1', 'snow2', 'tomato', 'red1', 'red2', 'red3', 'red4')) +
-#   labs(x = 'Allele length (motifs)', y = 'Reads', fill = 'Median methylation')  
-# ggsave('FMR1_PureTarget_methylation_byregion.pdf', width = 20, height = 20)
+# Combined plots for all samples -----------------------------------------------
